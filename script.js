710b93a61c25dc74a8b2d3e0ae9a1e60f6623d4e
samarth <84143644+Samarth-Nanhore@users.noreply.github.com>
2023-10-05 16:37:02 +0530

    Create function name operate in script.js
    This function would take three argument from variables name firstNumber, secondNumber and operator while calling and return result from mathematical functions name add, subtract, multiply and divide using conditional statement

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ let divide = (...arg) => {
 let firstNumber = 50;
 let secondNumber = 20;
 let operator = "*";
+
+let operate = (operator, firstNumber, secondNumber) => {
+  if (operator === "+") {
+    return add(firstNumber, secondNumber);
+  } else if (operator === "-") {
+    return subtract(firstNumber, secondNumber);
+  } else if (operator === "*") {
+    return multiply(firstNumber, secondNumber);
+  } else if (operator === "/") {
+    return divide(firstNumber, secondNumber);
+  } else {
+    return "No output";
+  }
+};
+
+console.log(operate(operator, firstNumber, secondNumber));
